896c96e160f1c1a63f333227ed8888e3c3f0ef2d
tannguyen1103@gmail.com <tannguyen1103@gmail.com>
2022-07-08 09:36:29 +0700

    THem ghi chu

--- src/routers/user/current-user.ts
+++ src/routers/user/current-user.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { currentUser } from '../../middlewares/current-user';
 
 const router = express.Router();
-
+// them test
 router.get('/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
